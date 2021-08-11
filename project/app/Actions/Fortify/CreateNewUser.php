9c2a8732635b9048400eb1c791fe7fdb78c7cd30
kevinNdyabandiho <59060080+kevinNdyabandiho@users.noreply.github.com>
2021-08-11 21:24:37 +0300

    third commit

--- project/app/Actions/Fortify/CreateNewUser.php
+++ project/app/Actions/Fortify/CreateNewUser.php
@@ -31,6 +31,9 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'role_id' => $input['role_id'],
+            'user_address' =>$input['user_address'],
+            
         ]);
     }
 }
--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -27,6 +27,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
+        'user_address',
+        
+
     ];
 
     /**

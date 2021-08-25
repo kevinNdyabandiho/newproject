23602b913b885341e729fe9e4a11b3be7a28eda4
kevinNdyabandiho <59060080+kevinNdyabandiho@users.noreply.github.com>
2021-08-25 18:12:21 +0300

    series one commit

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -20,10 +20,12 @@
             }
         </style>
     </head>
+    
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <nav class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
@@ -33,7 +35,7 @@
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
                         @endif
                     @endauth
-                </div>
+                </nav>
             @endif
 
             {{-- <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
